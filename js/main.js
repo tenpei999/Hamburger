7505fae40c8b7956ecae6316a952d0883e2c1f58
tenpei999 <tenpei999@gmail.com>
2022-08-07 21:19:28 +0900

    レスポンシブcontentsまで

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ jQuery(function( $ ) {
   $(".js-hamburger").on("click", function() {
     $( this ).toggleClass("is-open");
     $(".p-gmenu").toggleClass("is-open");
+    $(".p-gmenu--fead").toggleClass("is-open");
     $("body").toggleClass("is-open");
   });
 });
