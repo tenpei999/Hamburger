ca8e404ec26b6e105bd5f40382a313a5003707e9
tenpei999 <tenpei999@gmail.com>
2022-08-07 12:20:51 +0900

    ハンバーガー実装前夜祭

---
+++ js/main.js
@@ -0,0 +1,7 @@
+jQuery(function( $ ) {
+  $(".js-hamburger").on("click", function() {
+    $( this ).toggleClass("is-open");
+    $(".p-gmenu").toggleClass("is-open");
+    $("body").toggleClass("is-open");
+  });
+});
