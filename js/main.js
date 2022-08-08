2504a93491b526ccb6a1c94b9481f25546eb0c72
tenpei999 <tenpei999@gmail.com>
2022-08-08 16:37:49 +0900

    サイドバーのユーザビリティーを向上したい

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ jQuery(function( $ ) {
     $( this ).toggleClass("is-open");
     $(".p-gmenu").toggleClass("is-open");
     $(".p-gmenu--fead").toggleClass("is-open");
+    $(".p-gmenu--list").toggleClass("is-open");
     $("body").toggleClass("is-open");
   });
 });
