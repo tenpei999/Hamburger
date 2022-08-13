9721424e28a46b53781b59ee7f0761aebcf894ba
tenpei999 <tenpei999@gmail.com>
2022-08-13 20:40:09 +0900

    寄り道

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ jQuery(function( $ ) {
     $( this ).toggleClass("is-open");
     $(".p-gmenu").toggleClass("is-open");
     $(".p-gmenu--fead").toggleClass("is-open");
-    $(".p-gmenu--list").toggleClass("is-open");
+    $(".list").toggleClass("is-open");
     $("body").toggleClass("is-open");
   });
 });
