fe4ddb876d79d7e3b6b0c61d40638f9ba06da952
tenpei999 <109806883+tenpei999@users.noreply.github.com>
2022-08-14 13:33:53 +0900

    Simply (#1)
    * 寄り道
    
    * aside修正

--- js/main.js
+++ js/main.js
@@ -3,7 +3,21 @@ jQuery(function( $ ) {
     $( this ).toggleClass("is-open");
     $(".p-gmenu").toggleClass("is-open");
     $(".p-gmenu--fead").toggleClass("is-open");
-    $(".p-gmenu--list").toggleClass("is-open");
+    $(".list").toggleClass("is-open");
     $("body").toggleClass("is-open");
+    $("aside").toggleClass("is-open");
   });
+  // $(window).resize(function() {
+  //   let windowWidth = window.innerWidth;
+  //   let point = 1220;
+  //   if (windowWidth >= point)  //画面サイズが1200px以上のときの処理
+    
+    // $(".js-hamburger").removeClass("is-open");
+    // $("aside").removeClass("is-open");
+    // $(".p-gmenu").removeClass("is-open");
+    // $(".p-gmenu--fead").removeClass("is-open");
+    // $(".list").removeClass("is-open");
+    // $("body").removeClass("is-open"); 
+
+  // });
 });
