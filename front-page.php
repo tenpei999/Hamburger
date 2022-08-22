b0be60239c05c993212b30ddb0644c13e581a345
tenpei999 <tenpei999@gmail.com>
2022-08-23 00:37:14 +0900

    フロントページ

--- front-page.php
+++ front-page.php
@@ -1,3 +1,96 @@
 <?php get_header();?>
+  
+      <main class="l-main">
+
+        <div class="c-background-image p-main-visual">
+          <h1 class="c-text--M-white">
+            ダミーサイト
+          </h1>
+        </div>
+        <!-- main-visual-->
+
+        <div class="l-contents--wrapper">
+
+          <div class="l-contents p-contents__take-out c-background-image">
+            <h2 class="p-contents__section-title">
+              <p class="c-text--roboto-white u-under-bar">
+                Take out
+              </p>
+            </h2>       
+            <div class="l-contents__item p-contents__item c-background-color--white-opacity">
+              <h3 class="p-contents__contents-title c-text--M-text-black">
+                Take Out
+              </h3>
+              <p class="c-hidden-text">
+                当店のテイクアウトで利用できる商品を掲載しています。当店のテイクアウトで利用できる商品を掲載しています。当店のテイクアウトで利用できる商品を掲載しています。当店のテイクアウトで利用できる商品を掲載しています当店のテイクアウトで利用できる商品を掲載しています。当店のテイクアウトで利用できる商品を掲載しています。
+              </p>
+            </div>
+            <!-- contents-item take out -->
+            <div class="l-contents__item p-contents__item c-background-color--white-opacity">
+              <h3 class="p-contents__contents-title c-text--M-text-black">
+                Take Out
+              </h3>
+              <p>
+                当店のテイクアウトで利用できる商品を掲載しています。当店のテイクアウトで利用できる商品を掲載しています。当店のテイクアウトで利用できる商品を掲載しています。当店のテイクアウトで利用できる商品を掲載しています当店のテイクアウトで利用できる商品を掲載しています。当店のテイクアウトで利用できる商品を掲載しています。
+              </p>
+            </div>
+            <!--  contents-item take out -->
+          </div>
+          <!-- contents take out -->
+
+          <div class="l-contents p-contents__eat-in c-background-image">
+            <h2 class="p-contents__section-title">
+              <p class="c-text--roboto-white  u-under-bar">
+                Eat In
+              </p>
+            </h2>         
+            <div class="l-contents__item p-contents__item c-background-color--white-opacity">
+              <h3 class="p-contents__contents-title c-text--M-text-black">
+                Eat In
+              </h3>
+              <p>
+                店内でお食事いただけるメニューです。
+                店内でお食事いただけるメニューです。
+                店内でお食事いただけるメニューです。
+                店内でお食事いただけるメニューです。
+              </p>
+            </div>
+            <!-- contents-item eat in -->
+            <div class="l-contents__item p-contents__item c-background-color--white-opacity">
+              <h3 class="p-contents__contents-title c-text--M-text-black">
+                Eat In
+              </h3>
+              <p>
+                店内でお食事いただけるメニューです。
+                店内でお食事いただけるメニューです。
+                店内でお食事いただけるメニューです。
+                店内でお食事いただけるメニューです。
+              </p>
+            </div>
+            <!--  contents-item eat in -->
+          </div>
+          <!-- contents eat in -->
+        </div>
+        <!-- contents wrapper-->
+
+          <div class="l-about p-about">
+            <div class="l-about__inner p-about__inner">
+              <h2 class="p-about__section-title">
+                <p class="c-text--roboto-white  u-under-bar">
+                  見出しが入ります。
+                </p>
+              </h2>
+              <p class="c-text--M-white">
+                テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入りす。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入りす。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。
+              </p>
+            </div>
+          </div>
+          <!-- about -->
+      </main>
+
+    </div>
+    <!-- container -->
+  
 <?php get_sidebar();?>
-<?php get_footer();?>
+
+    <?php get_footer();?>
