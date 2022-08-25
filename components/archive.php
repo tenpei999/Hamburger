4078d47d2fe263b544e36234f5443e7ab0c016be
tenpei999 <tenpei999@gmail.com>
2022-08-25 20:04:22 +0900

    component/archive連携

--- components/archive.php
+++ components/archive.php
@@ -1,8 +1,9 @@
 <div class="l-card__inner">
-  <?php if (have_posts()) : while (have_post()) : the_post(); ?>
+  <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
       <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
         <?php the_category(', '); ?>
         <div class="p-card c-card">
+
           <?php the_post_thumbnail('post-thumbnail', array('alt' => the_title_attribute('echo=0'))); ?>
           <div class="c-card__contents-area">
             <div class="c-card__text-area">
