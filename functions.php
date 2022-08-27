a88de53b8f9aaf18bf4986d0ab0c651134d5a0fe
tenpei999 <tenpei999@gmail.com>
2022-08-27 13:55:35 +0900

    searchiページ, タグ検索およびカテゴリ検索実装

--- functions.php
+++ functions.php
@@ -206,3 +206,48 @@ function my_posts_control( $query ) {
   }
 }
 add_action( 'pre_get_posts', 'my_posts_control' );
+
+
+//サイト内検索のカスタマイズ
+function custom_search($search, $wp_query) {
+	global $wpdb;
+
+	//検索ページ以外だったら終了
+	if (!$wp_query->is_search)
+	return $search;
+
+	if (!isset($wp_query->query_vars))
+	return $search;
+
+	// タグ名・カテゴリ名・カスタムフィールド も検索対象にする
+	$search_words = explode(' ', isset($wp_query->query_vars['s']) ? $wp_query->query_vars['s'] : '');
+	if ( count($search_words) > 0 ) {
+		$search = '';
+		foreach ( $search_words as $word ) {
+			if ( !empty($word) ) {
+				$search_word = $wpdb->escape("%{$word}%");
+				$search .= " AND (
+						{$wpdb->posts}.post_title LIKE '{$search_word}'
+						OR {$wpdb->posts}.post_content LIKE '{$search_word}'
+						OR {$wpdb->posts}.ID IN (
+							SELECT distinct r.object_id
+							FROM {$wpdb->term_relationships} AS r
+							INNER JOIN {$wpdb->term_taxonomy} AS tt ON r.term_taxonomy_id = tt.term_taxonomy_id
+							INNER JOIN {$wpdb->terms} AS t ON tt.term_id = t.term_id
+							WHERE t.name LIKE '{$search_word}'
+						OR t.slug LIKE '{$search_word}'
+						OR tt.description LIKE '{$search_word}'
+						)
+						OR {$wpdb->posts}.ID IN (
+							SELECT distinct p.post_id
+							FROM {$wpdb->postmeta} AS p
+							WHERE p.meta_value LIKE '{$search_word}'
+						)
+				) ";
+			}
+		}
+	}
+
+	return $search;
+}
+add_filter('posts_search','custom_search', 10, 2);
