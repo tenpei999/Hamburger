388cc39699b664eeb7d52e6f35d71224cbd9fe07
tenpei999 <tenpei999@gmail.com>
2023-05-01 22:18:25 +0900

    [dist]jQuery削除

--- assets/scripts/bundle.js
+++ assets/scripts/bundle.js
@@ -1,11 +1,15 @@
-jQuery(function( $ ) {
-  $(".js-hamburger").on("click", function() {
-    $( this ).toggleClass("is-open");
-    $(".p-gmenu").toggleClass("is-open");
-    $(".p-gmenu--fead").toggleClass("is-open");
-    $(".list").toggleClass("is-open");
-    $("body").toggleClass("is-open");
-    $("aside").toggleClass("is-open");
-    $("footer").toggleClass("is-open");
+window.addEventListener("DOMContentLoaded", function() {
+  const hamburger = document.querySelector(".js-hamburger");
+  const pGmenuFead = document.querySelector(".p-gmenu--fead");
+  const body = document.querySelector("body");
+  const aside = document.querySelector("aside");
+  const footer = document.querySelector("footer");
+
+  hamburger.addEventListener("click", function() {
+    this.classList.toggle("is-open");
+    pGmenuFead.classList.toggle("is-open");
+    body.classList.toggle("is-open");
+    aside.classList.toggle("is-open");
+    footer.classList.toggle("is-open");
   });
 });
--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 import "./main.scss";
-import './assets/scripts/jquery-3.6.1.min.js';
 import './assets/scripts/bundle.js';
 import.meta.glob( [
 	'./assets/images/**',
