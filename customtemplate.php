9d60d95cff2ddd487cafebe2ca0b9d04fe40c63d
tenpei999 <tenpei999@gmail.com>
2023-05-12 22:54:09 +0900

    [feat]main-visual

--- customtemplate.php
+++ customtemplate.php
@@ -4,44 +4,9 @@
 
 <main class="l-main">
 
-  <section class="c-background-image p-main-visual single is-news">
+  <section class="c-background-image p-main-visual single is-news-top">
     <div class="p-main-visual__inner single"></div>
-    <h1 class="c-text--M-white"><?php the_title(); ?>
-
-      <?php
-      $taxonomy = 'news-category'; // カスタムタクソノミーのスラッグを指定する
-      $terms = get_terms($taxonomy);
-
-      if (!empty($terms) && !is_wp_error($terms)) {
-        echo '<ul class="c-news-archive-categories is-top">';
-        $count = count($terms);
-        foreach ($terms as $key => $term) {
-          $total_posts = $term->count;
-          if ($key === $count - 1) {
-            echo '<li class="c-news-archive-category is-top"><a href="' . esc_url(get_term_link($term)) . '">' . $term->name . '(' . $total_posts . ')' . '</a>
-          </li>';
-          } else {
-            echo '<li class="c-news-archive-category is-top"><a href="' . esc_url(get_term_link($term)) . '">' . $term->name . ' (' . $total_posts . ')' . '</a><p>,</P></li>';
-          }
-        }
-        echo '</ul>';
-      }
-      ?>
-
-      <?php
-      $taxonomy = 'news-tags'; // カスタムタクソノミーのスラッグを指定する
-      $terms = get_terms($taxonomy);
-
-      if (!empty($terms) && !is_wp_error($terms)) {
-        echo '<ul class="c-news-archive-tags is-top">';
-        foreach ($terms as $term) {
-          $total_posts = $term->count;
-          echo '<li class="c-news-archive-tag is-top"><a href="' . esc_url(get_term_link($term)) . '">' . $term->name . ' (' . $total_posts . ')' . '</a></li>';
-        }
-        echo '</ul>';
-      }
-      ?>
-    </h1>
+    <h1 class="c-text--M-white"><?php the_title(); ?></h1>
 
   </section>
   <!-- main-visual-->
--- single-news.php
+++ single-news.php
@@ -4,9 +4,42 @@
 
 <main class="l-main">
 
-  <section class="c-background-image p-main-visual single">
+  <section class="c-background-image p-main-visual single is-news">
     <div class="p-main-visual__inner single"></div>
-    <h1 class="c-text--M-white"><?php the_title(); ?></h1>
+    <h1 class="c-text--M-white"><?php the_title(); ?>
+      <?php
+      $terms = get_the_terms($post->ID, 'news-category');
+
+      if (!empty($terms) && !is_wp_error($terms)) {
+        echo '<ul class="c-news-archive-categories is-top">';
+        $count = count($terms);
+        foreach ($terms as $key => $term) {
+          $total_posts = $term->count;
+          if ($key === $count - 1) {
+            echo '<li class="c-news-archive-category is-mv"><a href="' . esc_url(get_term_link($term)) . '">' . $term->name . '(' . $total_posts . ')' . '</a>
+          </li>';
+          } else {
+            echo '<li class="c-news-archive-category is-mv"><a href="' . esc_url(get_term_link($term)) . '">' . $term->name . ' (' . $total_posts . ')' . '</a><p>,</P></li>';
+          }
+        }
+        echo '</ul>';
+      }
+      ?>
+
+      <?php
+      $terms = get_the_terms($post->ID, 'news-tags');
+
+      if (!empty($terms) && !is_wp_error($terms)) {
+        echo '<ul class="c-news-archive-tags is-mv">';
+        foreach ($terms as $term) {
+          $total_posts = $term->count;
+          echo '<li class="c-news-archive-tag is-mv"><a href="' . esc_url(get_term_link($term)) . '">' . $term->name . ' (' . $total_posts . ')' . '</a></li>';
+        }
+        echo '</ul>';
+      }
+      ?>
+
+    </h1>
   </section>
   <!-- main-visual-->
   <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
